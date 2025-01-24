e57be96ca7af8bd9b72840054c9742ad76c621c4
jonathan_fambro <startek591@gmail.com>
2025-01-24 10:27:33 -0500

    Created the template application to demonstrate rxjs + signal code

---
+++ simple/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit, OnDestroy {
+  name = 'Angular';
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {}
+}
