a63305fed2bf97e679dbe13f115756cae71fab45
jonathan_fambro <startek591@gmail.com>
2025-01-26 17:05:14 -0500

    Import HttpClient Module

--- angular-rxjs-signals-fundamentals/src/app/app.config.ts
+++ angular-rxjs-signals-fundamentals/src/app/app.config.ts
@@ -8,9 +8,11 @@ import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppData } from './app-data';
 import { FormsModule } from '@angular/forms';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     importProvidersFrom(FormsModule, InMemoryWebApiModule.forRoot(AppData)),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
