eb0fbdf946cc590d1be2d401d94d51adc3858b20
jonathan_fambro <startek591@gmail.com>
2025-01-26 17:37:18 -0500

    Subscribe to the Returned Observable in a Component

--- angular-rxjs-signals-fundamentals/src/app/products/product-list/product-list.component.ts
+++ angular-rxjs-signals-fundamentals/src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { NgIf, NgFor, NgClass } from '@angular/common';
+import { ProductService } from '../product.service';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'pm-product-list',
@@ -8,13 +10,30 @@ import { NgIf, NgFor, NgClass } from '@angular/common';
   standalone: true,
   imports: [NgIf, NgFor, NgClass],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle = 'Products';
   errorMessage = '';
+  sub!: Subscription;
+
+  private productService = inject(ProductService);
 
   products: Product[] = [];
 
   selectedProductId: number = 0;
+
+  ngOnInit(): void {
+    this.sub = this.productService
+      .getProducts()
+      .pipe(tap(() => console.log('In component pipeline')))
+      .subscribe((products) => {
+        this.products = products;
+        console.log('products', products);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;
