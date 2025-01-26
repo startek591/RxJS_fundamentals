bceca965fa4c1b50c216e90aab8b980d8fe03507
jonathan_fambro <startek591@gmail.com>
2025-01-26 16:22:11 -0500

    Add App Data and Forms Module

--- angular-rxjs-signals-fundamentals/src/app/app.config.ts
+++ angular-rxjs-signals-fundamentals/src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppData } from './app-data';
+import { FormsModule } from '@angular/forms';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    importProvidersFrom(FormsModule, InMemoryWebApiModule.forRoot(AppData)),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
