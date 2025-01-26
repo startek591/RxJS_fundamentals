ef587b4e7e6f4ebbf522aeb93b611c99383b9571
jonathan_fambro <startek591@gmail.com>
2025-01-26 17:36:58 -0500

    Retrieve an Array of Items in a Service

--- angular-rxjs-signals-fundamentals/src/app/products/product.service.ts
+++ angular-rxjs-signals-fundamentals/src/app/products/product.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  // Just enough here for the code to compile
   private productsUrl = 'api/products';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl).pipe(
+      tap(() => console.log('In http.get pipeline')),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
+  }
 }
