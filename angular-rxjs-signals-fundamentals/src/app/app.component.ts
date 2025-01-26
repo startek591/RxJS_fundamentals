2b1f08b6a73ae8d78b4592f48a78f99e4e3552b9
jonathan_fambro <startek591@gmail.com>
2025-01-26 15:18:14 -0500

    Added App Component Both Logic and Template

--- angular-rxjs-signals-fundamentals/src/app/app.component.ts
+++ angular-rxjs-signals-fundamentals/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterLinkActive, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-rxjs-signals-fundamentals';
+  pageTitle = 'Acme Product Management';
+
+  cartCount = 0;
 }
